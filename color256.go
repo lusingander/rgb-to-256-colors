86c9cb469dded217f14f840099ddd30680e7641b
fujimoto kyosuke <kyoro.f@gmail.com>
2019-12-31 07:48:00 +0900

    merge files

--- color256.go
+++ color256.go
@@ -1,6 +1,11 @@
 package rgbto256colors
 
-import "image/color"
+import (
+	"image/color"
+	"math"
+
+	"github.com/mattn/go-ciede2000"
+)
 
 type Color256 struct {
 	ColorID   uint8
@@ -16,4 +21,18 @@ func (c *Color256) ToColor() color.Color {
 		B: c.B,
 		A: 255,
 	}
+}
+
+func FromRGB(r, g, b uint8) *Color256 {
+	t := &color.RGBA{r, g, b, 255}
+	minDiff := math.MaxFloat64
+	var bestColor *Color256
+	for _, c := range colors {
+		d := ciede2000.Diff(c.ToColor(), t)
+		if d < minDiff {
+			minDiff = d
+			bestColor = c
+		}
+	}
+	return bestColor
 }
